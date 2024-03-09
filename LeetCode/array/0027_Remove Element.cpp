1c8e634720105bb9bb630e2526e0cce862917310
ZhouTao415 <dylanzhou415@gmail.com>
2024-03-09 17:00:31 +0100

    add the comment in 0027

--- LeetCode/array/0027_Remove Element.cpp
+++ LeetCode/array/0027_Remove Element.cpp
@@ -1,2 +1,8 @@
-// vector.size()
-// vector.erase() -> time complexity O(n)
+/* 
+vector.size()
+vector.erase() -> time complexity O(n)
+Solution: Two Pointer Approach 双指针
+fastIndex:获取新数组中的元素 
+slowIndex：获取系数组中需要更新的位置 
+if 是更新数组的一个操作
+*/
